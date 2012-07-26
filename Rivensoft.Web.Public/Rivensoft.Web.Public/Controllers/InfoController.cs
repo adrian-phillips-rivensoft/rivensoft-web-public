0e0ba2913fb007e66f83a88fdcfa389f69a0d947
Adrian Phillips <adrian.phillips@rivensoft.com>
2012-07-26 16:43:51 +0100

    Added the client and info controllers. Played around with the colour schemes. Added some dummy text.

---
+++ Rivensoft.Web.Public/Rivensoft.Web.Public/Controllers/InfoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rivensoft.Web.Public.Controllers
+{
+    public class InfoController : Controller
+    {
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        
+    }
+}
